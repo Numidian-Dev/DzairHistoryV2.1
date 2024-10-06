96ae74a53adeb74e9a7e1d477bdd9d1f62425ec2
Adel <adel@MacBook-Air-de-Adel.local>
2024-10-06 18:47:10 +0200

    remove page2

--- src/app/components/config/URL.js
+++ src/app/components/config/URL.js
@@ -1,6 +1,6 @@
 const dev = process.env.NODE_ENV !== "production";
 const url_api = dev
     ? "http://localhost:5050"
-    : "https://dzairhistory.com"
+    : "https://dzairhistory.com";
 
 export default url_api;
