1c225dcd48da13ed9c558f47abbbe3de42e154ca
Adel <numidian.dev@gmail.com>
2024-10-11 15:23:37 +0200

    remove insta

--- src/app/page.js
+++ src/app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
         <div className="main-contents">
           <OtherArticles/>
           <Books/>
-         <Instagram/> 
+        {/*  <Instagram/>  */}
         </div>
         <aside>
           <SideBar/>
