fa41bf3a08b31dc44f3290e14977ad2fcf5e7aa6
Adel <numidian.dev@gmail.com>
2024-11-08 15:14:47 +0100

    article cirta

--- backend/api/data.js
+++ backend/api/data.js
@@ -401,7 +401,27 @@ const data = [
       img_profil: "",
       lang: "fr",
     },
-    
+    {
+      id: 21,
+      title: "Stèles de Cirta : Vestiges de la présence des communautés grecques et latines dans la capitale numide",
+      body: "<p class='firstP'>Cirta, l'antique capitale du royaume de Numidie, aujourd'hui Constantine en Algérie, était un centre urbain où se croisaient diverses cultures.</p> <p>Les Berbères y vivaient aux côtés de populations étrangères, tels que les Puniques, les Grecs et les Romano-Italiens. Ces étrangers jouaient des rôles variés : Massinissa, par exemple, fit appel à des musiciens grecs, tandis que son fils bénéficia d'une éducation hellénique auprès de philosophes grecs. Son fils et successeur, Micipsa, fréquenta un cercle d’érudits grecs et s’intéressa à diverses disciplines, dont la philosophie. On trouvait également dans la capitale numide des mercenaires et des prêtres.</p> <h2>Les stèles comme témoignages archéologiques</h2> <p>Les découvertes archéologiques sur la colline d'El Hofra, effectuées entre 1875 et 1950, ont mis au jour de nombreuses stèles, dont certaines remontent à l'époque de Massinissa. Ces vestiges, portant des inscriptions en grec et en latin, constituent des témoignages précieux des pratiques religieuses et culturelles de l'époque. Les dédicaces gravées sur ces stèles nous renseignent sur la composition sociale de la cité et les interactions entre les différentes communautés.</p> <h2>Conservation et héritage culturel</h2> <p>Cette présence suggère des interactions fréquentes avec des cultures étrangères, que ce soit dans le commerce ou les échanges intellectuels, contribuant ainsi à la richesse culturelle de cette époque.</p> <p>Ces stèles de Cirta offrent un aperçu unique sur l’influence des cultures grecques et latines en Numidie, faisant de Constantine un centre historique d’interaction culturelle préservé au Musée national Cirta et au Louvre.</p> <div class='stele'> <img src='/article/Constantine/stele-cirta.png'> </div> <p class='src-img'> © 2010 Musée du Louvre / Thierry Ollivier</p> <div class='hr-source'> </div><p class='src'>Source: Bertrandy, F. (1985). La communauté gréco-latine de Cirta (Constantine), capitale du royaume de Numidie, pendant le IIe siècle et la première moitié du Ier siècle avant J.-C. Latomus, 44(3), 488-502 </p> <style> .stele { width: 100%; } .stele img { width: 100%; height: 100%; object-fit: cover; } @media screen and (max-width: 677px) { .stele { width: 90%; margin : auto; margin-top:10px } } </style>",
+      resumer:
+        "SCirta, l'antique capitale du royaume de Numidie, aujourd'hui Constantine en Algérie, était un centre urbain où se croisaient diverses cultures. Les Berbères y vivaient aux côtés de populations étrangères, tels que les Puniques, les Grecs et les Romano-Italiens. Ces étrangers jouaient des rôles variés : Massinissa, par exemple, fit appel à des musiciens grecs, tandis que son fils bénéficia d'une éducation hellénique auprès de philosophes grecs. Son fils et successeur, Micipsa, fréquenta un cercle d’érudits grecs et s’intéressa à diverses disciplines, dont la philosophie. On trouvait également dans la capitale numide des mercenaires et des prêtres.",
+      img: "/article/Constantine/cirta.png",
+      imgLastPost: "/article/Constantine/cirta.png",
+      credit: "© 2010 Musée du Louvre / Thierry Ollivier",
+      alt: "Cirta",
+      categorie: "Histoire",
+      wilaya: "Constantine",
+      dynastie: "Numidie",
+      epoque: -200,
+      link: "steles-de-cirta",
+      meta: "/img/meta/Constantine/cirta-meta.png",
+      date: "14/09/2023",
+      contributeur: "Kutami",
+      img_profil: "",
+      lang: "fr",
+    },
   ];
   
   module.exports = data;
