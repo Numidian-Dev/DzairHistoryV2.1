b73b6c0a21ba76f71a466ac6a2fa85116b854ced
Adel <numidian.dev@gmail.com>
2025-09-08 22:00:12 +0200

    test 4

--- src/app/components/SideBar/SideBar.jsx
+++ src/app/components/SideBar/SideBar.jsx
@@ -1,20 +1,44 @@
+"use client";
+
+import { useEffect } from "react";
 import Script from "next/script";
 import TitleSection from "../titleSection/TitleSection";
 
 const SideBar = () => {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      try {
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
+      } catch (e) {
+        console.error("Adsense error", e);
+      }
+    }
+  }, []);
+
   return (
     <section className="aside">
-      <TitleSection title={"Ads"} backgroundColor={"#151414"} />
-        <div id="script-pub">
-          <Script
-            id="test-id"
-            async="async"
-            data-cfasync="false"
-            src="//pl18195204.highrevenuegate.com/c1ae8d1ce9929d8adb4afa2c5b3581ef/invoke.js"
-          />
-          <div id="container-c1ae8d1ce9929d8adb4afa2c5b3581ef"></div>
+      <TitleSection title="Ads" backgroundColor="#151414" />
+
+      <div id="script-pub">
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4426518773218222"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+
+        <ins
+          className="adsbygoogle"
+          style={{ display: "block" }}
+          data-ad-client="ca-pub-4426518773218222"
+          data-ad-slot="2411959502"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+          
+        ></ins>
       </div>
     </section>
   );
 };
+
 export default SideBar;
