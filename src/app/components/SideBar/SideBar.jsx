d755c4d5d0f983a619e3b915598fdb8c9ba806c2
Adel <numidian.dev@gmail.com>
2025-09-08 22:03:46 +0200

    double  ads

--- src/app/components/SideBar/SideBar.jsx
+++ src/app/components/SideBar/SideBar.jsx
@@ -37,6 +37,24 @@ const SideBar = () => {
           
         ></ins>
       </div>
+      <div id="script-pub">
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4426518773218222"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+
+        <ins
+          className="adsbygoogle"
+          style={{ display: "block" }}
+          data-ad-client="ca-pub-4426518773218222"
+          data-ad-slot="2411959502"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+          
+        ></ins>
+      </div>
     </section>
   );
 };
